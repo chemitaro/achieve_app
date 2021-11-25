6dc1643e38e04fd64a24b98d8a766b2cb5751ad9
yuta iwasawa <iwasi_44@hotmail.com>
2021-11-25 11:23:19 +0900

    babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -67,6 +67,12 @@ module.exports = function(api) {
           async: false
         }
       ]
+      [
+        "@babel/plugin-proposal-private-methods",
+         { 
+           "loose": true 
+          }
+      ]
     ].filter(Boolean)
   }
 }
